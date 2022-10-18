fe6780d344a5f364cfa1a4d46f75975e28fdfdcf
abdc06 <abdc2806@gmail.com>
2022-10-19 05:37:29 +0900

    클래스로더 네임 검증 및 출력

--- class-loader/src/main/java/me/abdc/Application.java
+++ class-loader/src/main/java/me/abdc/Application.java
@@ -2,6 +2,21 @@ package me.abdc;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ClassLoader classLoader = Application.class.getClassLoader(); // AppClassLoader
+        ClassLoader parentClassLoader = classLoader.getParent(); // PlatformClassLoader
+        ClassLoader parentParentClassLoader = parentClassLoader.getParent(); // BootstrapClassLoader
+
+        assert classLoader != null;
+        assert parentClassLoader != null;
+        assert parentParentClassLoader == null;
+
+        assert "jdk.internal.loader.ClassLoaders$AppClassLoader".equals(classLoader.getClass().getName());
+        assert "jdk.internal.loader.ClassLoaders$PlatformClassLoader".equals(parentClassLoader.getClass().getName());
+        assert parentParentClassLoader == null;
+
+        System.out.println(classLoader);
+        System.out.println(parentClassLoader);
+        System.out.println(parentParentClassLoader);
     }
 }
