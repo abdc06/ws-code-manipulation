487674e5edea7ccf0a5b18e4d75872142ec1039f
abdc06 <abdc2806@gmail.com>
2022-10-29 17:01:57 +0900

    리플렉션을 이용한 클래스 정보 및 실행 확인

--- java-reflection/src/main/java/me/abdc/Application.java
+++ java-reflection/src/main/java/me/abdc/Application.java
@@ -1,7 +1,59 @@
 package me.abdc;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Class<MyClass> myClassClass = MyClass.class;
+        MyClass myClass = new MyClass();
+
+        System.out.println("--------------- MyClass Superclass ---------------");
+        System.out.println(myClassClass.getSuperclass());
+        System.out.println();
+
+
+        System.out.println("--------------- MyClass Interfaces ---------------");
+        Arrays.stream(myClassClass.getInterfaces()).forEach(System.out::println);
+        System.out.println();
+
+
+        System.out.println("--------------- MyClass DeclaredFields ---------------");
+        Arrays.stream(myClassClass.getDeclaredFields()).forEach(f -> {
+            try {
+                f.setAccessible(true);
+                System.out.printf("%s ==> %s\n", f, f.get(myClass));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println();
+
+
+        System.out.println("--------------- MyClass DeclaredConstructors ---------------");
+        Arrays.stream(myClassClass.getDeclaredConstructors()).forEach(System.out::println);
+        System.out.println();
+
+
+        System.out.println("--------------- MyClass DeclaredFields ---------------");
+        Arrays.stream(myClassClass.getDeclaredMethods()).forEach(m -> {
+            m.setAccessible(true);
+            System.out.println(m);
+        });
+        System.out.println();
+
+        System.out.println("--------------- MyClass Annotations ---------------");
+        Arrays.stream(myClassClass.getAnnotations()).forEach(System.out::println);
+        System.out.println();
+
+
+        System.out.println("--------------- MyClass Method Annotations ---------------");
+        Method method = myClassClass.getMethod("sum", int.class, int.class);
+        Arrays.stream(method.getAnnotations()).forEach(System.out::println);
+
+        Integer invoke = (Integer) method.invoke(myClass, 10, 20);
+        System.out.printf("%s %d\n", "MyClass sum invoke =>", invoke);
+        System.out.println();
     }
 }
