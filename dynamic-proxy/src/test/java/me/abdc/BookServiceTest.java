c5ade98d3e11a4e3db5c8e7f46de5c905939fb02
abdc06 <abdc2806@gmail.com>
2022-10-30 12:26:57 +0900

    low level의 다이나믹 프록시

--- dynamic-proxy/src/test/java/me/abdc/BookServiceTest.java
+++ dynamic-proxy/src/test/java/me/abdc/BookServiceTest.java
@@ -2,19 +2,36 @@ package me.abdc;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Proxy;
+
 class BookServiceTest {
 
     BookService bookService = new DefaultBookService();
 
     BookService bookServiceProxy = new BookServiceProxy(bookService);
 
+    BookService bookServiceDynamicProxy = (BookService) Proxy.newProxyInstance(
+            BookService.class.getClassLoader(),
+            new Class[]{BookService.class},
+            (proxy, method, args) -> {
+                System.out.println("before");
+                Object invoke = method.invoke(bookService, args);
+                System.out.println("after");
+                return invoke;
+            });
+
     @Test
     void rent() {
         Book book = new Book();
         book.setTitle("Proxy");
         bookService.rent(book);
+        System.out.println();
 
         bookServiceProxy.rent(book);
+        System.out.println();
+
+        bookServiceDynamicProxy.rent(book);
+        System.out.println();
     }
 
 }
